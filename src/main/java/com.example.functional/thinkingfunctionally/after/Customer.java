5d421fce4e78033b41a48f7363f4418f37c9ed26
vojha <vikas.ojha@soprabanking.com>
2023-07-02 13:06:00 +0530

    applying referential transparency

--- src/main/java/com.example.functional/thinkingfunctionally/after/Customer.java
+++ src/main/java/com.example.functional/thinkingfunctionally/after/Customer.java
@@ -6,6 +6,10 @@ public class Customer {
 
     private Integer rewardBalance;
 
+
+    // calling updateBalanceReward with same order twice will result in different output
+    // this is against pure functions as functions only return same output with same input
+    // so will move this to process reward class
     public void updateBalanceReward(Order order) {
        if(order.getOrderRewards() != null) {
             this.addToRewardBalance(order.getOrderRewards());
